a423558919f7ea927fac43ba6b8abf7288542839
Brandon Garcia <brandongarcia2332@gmail.com>
2015-07-23 19:12:11 -0700

    First Draft Video Game

--- videoGame.js
+++ videoGame.js
@@ -1,2 +1,123 @@
-console.log("test");
-console.log("test2")
+var canvas = document.createElement("canvas");
+car ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+//background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function(){
+    bgReady = true;
+};
+bgImage.src = "images/background.png";
+
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function(){
+    heroReady = true;
+};
+heroImage.src = "https://github.com/lostdecade/simple_canvas_game/blob/master/images/hero.png";
+
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function(){
+    monsterReady = true;
+};
+monsterImage.src = "https://github.com/lostdecade/simple_canvas_game/blob/master/images/monster.png";
+
+//game objects
+var hero = {
+    speed: 256,
+    x: 0,
+    y: 0
+};
+var monster ={
+    x: 0,
+    y: 0
+}
+var monstersCaught = 0;
+
+//handle keyboard controls
+var keysDown = {};
+addEventListener("keydown", function(e){
+    keysDown[e.keyCode] = true;
+    }, false);
+
+addEventListener("keyup", function(e){
+    delete keysDown[e.keyCode];
+    }, false);
+
+//Reset the game when player catches monster
+
+var reset = function(){
+    hero.x = canvas.width/2;
+    hero.y = canvas.height/2;
+    monster.x = 32 + (Math.random() * (canvas.width - 64));
+    monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+//update game objects
+
+var update = function(modifier){
+    if(38 in keysDown){
+        hero.y -= hero.speed * modifier;
+    }
+    if(40 in keysDown){
+        hero.y += hero.speed * modifier;
+    }
+    if(37 in keysDown){
+        hero.x -= hero.speed * modifier;
+    }
+    if(39 in keysDown){
+        hero.x += hero.speed * modifier;
+    }
+    if(
+        hero.x <= (monster.x + 32)
+        && monster.x <= (hero.x + 32)
+        && hero.y <= (monster.y + 32)
+        && monster.y <= (hero.y + 32)
+        ){
+         ++monstersCaught;
+         reset();
+    }
+}
+
+// draw everything
+
+var render = function(){
+    if(bgReady){
+        ctx.drawImage(bgImage, 0, 0);
+    }
+    if(heroReady){
+        ctx.drawImage(heroImage, hero.x, hero.y);
+    }
+    if(monsterReady){
+        ctx.drawImage(monsterImage, monster.x, monster.y)
+    }
+    ctx.fillStyle = "rgb(250,250,250)";
+    ctx.font = "24px Helvetica";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+};
+
+//main gain loop
+var main = function(){
+    var now = Date.now();
+    var delta = now - then;
+    update(delta/1000);
+    render();
+    then = now;
+    requestAnimationFrame(main);
+};
+
+//cross-browswer support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+//lets play the game
+
+var then = Date.now();
+reset();
+main();
